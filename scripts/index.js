b8b2926aabf996e64e606da741fe8dc2c2295212
Magdiel <magdiel.furtunato@aluno.senai.br>
2026-09-08 20:57:56 -0300

    Vinculaçaõ de json com o index.js

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+const dadosSalvos = localStorage.getItem("dadosDoAluno");
+const objetoFinal = JSON.parse(dadosSalvos);
+console.log(objetoFinal.length);
+
+let total_cadastrados=document.getElementById("total_cadastrados");
+total_cadastrados.textContent=objetoFinal.length;
+
+let alerta_estoque=document.getElementById("alerta_estoque");
+let quantidade_estoque=0;
+for (let i=0;i<objetoFinal.length;i++){
+    if (objetoFinal[i].Quantidade==1){
+        quantidade_estoque+=1;
+    }
+}
+alerta_estoque.textContent=quantidade_estoque;
+
+
